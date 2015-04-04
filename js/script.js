df95b01b13d4e8d4cf87066ffe7b0e9cbf25fd92
Anken <anken@oqy.be>
2015-04-05 01:00:27 +0200

    improve calcul

--- js/script.js
+++ js/script.js
@@ -6,25 +6,34 @@ $(document).ready(function($){
         $html = $('html'),
         height = $(window).height();
     // enlarge the chinese column until bottom of screen
-    var i = 0;
     $cc.hide();
     $cr.hide();
-    while (($html.height() < height) && (i < 50)) {
-        i++;
-        console.log($html.height(), height);
-        $html.css('font-size', (100 + (i*5)) + '%');
-    }    
-    $html.css('font-size', (100 + ((i-1) *5)) + '%');
+    if ((height < $html.height())) { 
+        var i = 0;
+        while ((height < $html.height()) && (i < 50)) {
+            i++;
+            console.log($html.height(), height);
+            $html.css('font-size', (100 - (i * 5)) + '%');
+        }  
+    } else {  
+    var i = 0;
+        while (($html.height() < height) && (i < 50)) {
+            i++;
+            console.log($html.height(), height);
+            $html.css('font-size', (100 + (i * 5)) + '%');
+        }    
+        $html.css('font-size', (97 + (i * 5)) + '%');
+    }
     $cc.show();
     $cr.show();
-    // reduce left columnt until less than chinese column
+    // reduce left column until less than chinese column
     var i = 50;
     while (($cl.height() < $cr.height()) && (0 < i)) {
         i--;
         console.log($cl.height(), $cr.height());
         $cr.css('font-size', (i*2) + '%');
     }    
-    // reduce center columnt until less than chinese column
+    // reduce center column until less than chinese column
     var i = 100;
     while (($cl.height() < $cc.height()) && (0 < i)) {
         i--;
